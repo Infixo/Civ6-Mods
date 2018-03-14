7230e4bd65c05d8dcab52d1429c8ae19dcb08330
Infixo <swidwins@gmail.com>
2018-03-14 23:13:55 +0100

    RCV: Version 0.5 page history

--- RCV/CivilopediaScreen_RCP.lua
+++ RCV/CivilopediaScreen_RCP.lua
@@ -1,10 +1,11 @@
-print("Loading CivilopediaScreen_RCP.lua from Real Civilopedia, version 0.3");
+print("Loading CivilopediaScreen_RCP.lua from Real Civilopedia, version 0.5");
 --------------------------------------------------------------
 -- Real Civilopedia
 -- Author: Infixo
 -- 2018-02-18: Created, raw modifier analysis
 -- 2018-02-22: Remember last visited page (based on CQUI code)
 -- 2018-03-07: Added Civs, Leaders, Units, Great People and reworked City-States
+-- 2018-03-14: Added page history
 --------------------------------------------------------------
 
 -- exposed functions and variables
@@ -15,12 +16,12 @@ local RMA = ExposedMembers.RMA;
 include("CivilopediaScreen");
 
 -- Cache base functions
-BASE_OnClose = OnClose;
-BASE_OnOpenCivilopedia = OnOpenCivilopedia;
-BASE_PageLayouts = {};
+RCP_BASE_OnOpenCivilopedia = OnOpenCivilopedia;
+RCP_BASE_NavigateTo = NavigateTo;
+RCP_BASE_PageLayouts = {};
 --print("Storing contents of PageLayouts:");
 for k,v in pairs(PageLayouts) do
-	BASE_PageLayouts[k] = v;
+	RCP_BASE_PageLayouts[k] = v;
 end
 
 
@@ -33,22 +34,98 @@ local _LastPageId = nil;
 function OnOpenCivilopedia(sectionId_or_search, pageId)
 	-- Opened without any query, restore the previously opened page and section instead
 	if sectionId_or_search == nil and _LastPageId then
-		print("Received a request to open the Civilopedia - last section and page");
-		NavigateTo(_LastSectionId, _LastPageId);
+		print("Received a request to open the Civilopedia - last section and page", _LastSectionId, _LastPageId);
+		NavigateTo(_LastSectionId, _LastPageId, true); -- should already be in history, so don't store again
 		UIManager:QueuePopup(ContextPtr, PopupPriority.Current);	
 		UI.PlaySound("Civilopedia_Open");
 	else
-		BASE_OnOpenCivilopedia(sectionId_or_search, pageId);
+		print("Received a request to open the Civilopedia");
+		pageVisitHistory = {};
+		pageHistoryIndex = 0;
+		RCP_BASE_OnOpenCivilopedia(sectionId_or_search, pageId);
 	end
 	Controls.SearchEditBox:TakeFocus();
 end
 
-function OnClose()
-	-- Store the currently opened section and page
-	_LastSectionId = _CurrentSectionId;
-	_LastPageId = _CurrentPageId;
-	BASE_OnClose();
+
+--------------------------------------------------------------
+-- 2018-03-14: Page history and back/next buttons (based on Civilopedia Improvement mod)
+
+-- stores the history of pages visited, with sectionID and pageID
+local pageVisitHistory = {};
+local pageHistoryIndex = 0;
+
+function IsBackPageButtonDisabled()
+	return #pageVisitHistory == 0 or pageHistoryIndex == 1;
 end
+
+function IsNextPageButtonDisabled()
+	return #pageVisitHistory == 0 or pageHistoryIndex == #pageVisitHistory;
+end
+
+function RefreshHistoryButtons()
+	Controls.BackPageButton:SetDisabled( IsBackPageButtonDisabled() );
+	Controls.NextPageButton:SetDisabled( IsNextPageButtonDisabled() );
+	Controls.BackPageButton:SetAlpha( IsBackPageButtonDisabled() and 0.4 or 1.0);
+	Controls.NextPageButton:SetAlpha( IsNextPageButtonDisabled() and 0.4 or 1.0);
+end
+
+-------------------------------------------------------------------------------
+-- This function will remember the history by default
+-- Need 3rd param set to tru to NOT rememeber
+-- This way we don't need to modify all functions that call NavigateTo
+function NavigateTo(SectionId, PageId, bNotInHistory)
+	--print("Navigating to " .. SectionId .. ":" .. PageId);
+
+	local prevSectionId = _CurrentSectionId;
+	local prevPageId = _CurrentPageId;
+
+	-- Store the currently opened section and page
+	_LastSectionId = SectionId;
+	_LastPageId = PageId;
+	
+	RCP_BASE_NavigateTo(SectionId, PageId);
+
+	if SectionId == prevSectionId and PageId == prevPageId then return; end
+
+	-- support for page history
+	if bNotInHistory then return; end
+	while pageHistoryIndex < #pageVisitHistory do
+		table.remove(pageVisitHistory);
+	end
+	table.insert(pageVisitHistory, { sectionId = SectionId, pageId = PageId });
+	pageHistoryIndex = #pageVisitHistory;
+	RefreshHistoryButtons();
+	--print("pageHistoryIndex:", pageHistoryIndex);
+	--for i,v in pairs(pageVisitHistory) do
+		--print(i .. ":" .. v.sectionId .. ":" .. v.pageId);
+	--end
+end
+
+function OnBackPageButton()
+	if IsBackPageButtonDisabled() then return; end -- assert
+	--print("Back button clicked");
+	pageHistoryIndex = pageHistoryIndex - 1;
+	NavigateTo( pageVisitHistory[ pageHistoryIndex ].sectionId, pageVisitHistory[ pageHistoryIndex ].pageId, true ); -- don't store
+	RefreshHistoryButtons();
+end
+
+function OnNextPageButton()
+	if IsNextPageButtonDisabled() then return; end -- assert
+	--print("Next button clicked");
+	pageHistoryIndex = pageHistoryIndex + 1;
+	NavigateTo( pageVisitHistory[ pageHistoryIndex ].sectionId, pageVisitHistory[ pageHistoryIndex ].pageId, true ); -- don't store
+	RefreshHistoryButtons();
+end
+
+function RCP_Initialize()
+	Controls.BackPageButton:RegisterCallback( Mouse.eLClick, OnBackPageButton );
+	Controls.BackPageButton:RegisterCallback( Mouse.eMouseEnter, function() UI.PlaySound("Main_Menu_Mouse_Over"); end );
+	Controls.NextPageButton:RegisterCallback( Mouse.eLClick, OnNextPageButton );
+	Controls.NextPageButton:RegisterCallback( Mouse.eMouseEnter, function() UI.PlaySound("Main_Menu_Mouse_Over"); end );
+	RefreshHistoryButtons();
+end
+RCP_Initialize();
 
 
 --------------------------------------------------------------
@@ -81,7 +158,7 @@ local tPagesToSkip:table = {
 -- add internal info to all pages at once
 function ShowPage(page)
 	--print("...showing page layout", page.PageLayoutId);
-	BASE_PageLayouts[page.PageLayoutId](page); -- call original function
+	RCP_BASE_PageLayouts[page.PageLayoutId](page); -- call original function
 	if tPagesToSkip[ page.PageLayoutId ] then return; end
 
 	local sImpact, tYields, sToolTip = RMA.CalculateModifierEffect(page.PageLayoutId, page.PageId, Game.GetLocalPlayer(), nil);
@@ -103,7 +180,7 @@ end
 
 PageLayouts["GreatPerson"] = function(page)
 	print("...showing page layout", page.PageLayoutId);
-	BASE_PageLayouts[page.PageLayoutId](page); -- call original function
+	RCP_BASE_PageLayouts[page.PageLayoutId](page); -- call original function
 	-- we need to show (a) GreatPersonIndividualActionModifiers (b) GreatPersonIndividualBirthModifiers
 	local sImpact, tYields, sToolTip = RMA.CalculateModifierEffect("GreatPersonIndividual", page.PageId, Game.GetLocalPlayer(), nil);
 	local chapter_body = {};
@@ -122,7 +199,7 @@ end
 -- UnitAbilities.UnitAbilityType=ABILITY_VARU
 PageLayouts["Unit"] = function(page)
 	print("...showing page", page.PageLayoutId, page.PageId);
-	BASE_PageLayouts[page.PageLayoutId](page); -- call original function
+	RCP_BASE_PageLayouts[page.PageLayoutId](page); -- call original function
 	
 	-- start with page.PageId, it contains UnitType
 	-- built ability list
@@ -163,7 +240,7 @@ AddUniques("Improvements");
 
 PageLayouts["Civilization"] = function(page)
 	print("...showing page", page.PageLayoutId, page.PageId);
-	BASE_PageLayouts[page.PageLayoutId](page); -- call original function
+	RCP_BASE_PageLayouts[page.PageLayoutId](page); -- call original function
 	
 	-- iterate through Traits that are not Uniques
 	for row in GameInfo.CivilizationTraits() do
@@ -180,7 +257,7 @@ end
 
 PageLayouts["Leader"] = function(page)
 	print("...showing page", page.PageLayoutId, page.PageId);
-	BASE_PageLayouts[page.PageLayoutId](page); -- call original function
+	RCP_BASE_PageLayouts[page.PageLayoutId](page); -- call original function
 	
 	-- iterate through Traits
 	for row in GameInfo.LeaderTraits() do
@@ -198,7 +275,7 @@ end
 
 PageLayouts["CityState"] = function(page)
 	print("...showing page layout", page.PageLayoutId);
-	BASE_PageLayouts[page.PageLayoutId](page); -- call original function
+	RCP_BASE_PageLayouts[page.PageLayoutId](page); -- call original function
 	--ShowModifiers(page);
 	-- special CityState logic
 	-- we need to show (a) leader traits (b) city-state type modifiers
