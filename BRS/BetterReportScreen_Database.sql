6254aa606221f3e3fa5db13fb411a5ca0329976e
Infixo <swidwins@gmail.com>
2018-03-24 01:45:16 +0100

    BRS: Version 3.2

--- BRS/BetterReportScreen_Database.sql
+++ BRS/BetterReportScreen_Database.sql
@@ -6,4 +6,4 @@
 
 -- just to make versioning easier
 INSERT INTO GlobalParameters (Name, Value) VALUES ('BRS_VERSION_MAJOR', '3');
-INSERT INTO GlobalParameters (Name, Value) VALUES ('BRS_VERSION_MINOR', '1');
+INSERT INTO GlobalParameters (Name, Value) VALUES ('BRS_VERSION_MINOR', '2');
