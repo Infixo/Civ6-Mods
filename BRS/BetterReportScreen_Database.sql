fbd869b0329748a9f9659c55a4eb9b32877fb310
Infixo <swidwins@gmail.com>
2018-03-25 20:33:24 +0200

    BRS: changed to version 3.3

--- BRS/BetterReportScreen_Database.sql
+++ BRS/BetterReportScreen_Database.sql
@@ -6,4 +6,4 @@
 
 -- just to make versioning easier
 INSERT INTO GlobalParameters (Name, Value) VALUES ('BRS_VERSION_MAJOR', '3');
-INSERT INTO GlobalParameters (Name, Value) VALUES ('BRS_VERSION_MINOR', '2');
+INSERT INTO GlobalParameters (Name, Value) VALUES ('BRS_VERSION_MINOR', '3');
