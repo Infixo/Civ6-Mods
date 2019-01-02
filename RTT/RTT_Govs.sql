27582b94e6ea66aea56e31cb378ce05c50274809
Infixo <swidwins@gmail.com>
2019-01-02 16:47:57 +0100

    RTT: Version 2.4
    Civics tweaks. Govs placement.

---
+++ RTT/RTT_Govs.sql
@@ -0,0 +1,19 @@
+--------------------------------------------------------------
+-- Real Tech Tree
+-- Optional - placement of Governments
+-- Author: Infixo
+-- 2019-01-02: Created
+--------------------------------------------------------------
+
+-- Change this parameter to '1' to activate balanced Government placement
+INSERT INTO GlobalParameters (Name, Value) VALUES ('RTT_OPTION_GOVS', '0');
+
+
+-- Autocracy
+UPDATE Governments SET PrereqCivic = 'CIVIC_GAMES_RECREATION' WHERE GovernmentType = 'GOVERNMENT_AUTOCRACY' AND EXISTS (SELECT * FROM GlobalParameters WHERE Name = 'RTT_OPTION_GOVS' AND Value = '1');
+
+-- Classical Republic
+UPDATE Governments SET PrereqCivic = 'CIVIC_DRAMA_POETRY' WHERE GovernmentType = 'GOVERNMENT_CLASSICAL_REPUBLIC' AND EXISTS (SELECT * FROM GlobalParameters WHERE Name = 'RTT_OPTION_GOVS' AND Value = '1');
+
+-- Monarchy
+UPDATE Governments SET PrereqCivic = 'CIVIC_DIPLOMATIC_SERVICE' WHERE GovernmentType = 'GOVERNMENT_MONARCHY' AND EXISTS (SELECT * FROM GlobalParameters WHERE Name = 'RTT_OPTION_GOVS' AND Value = '1');
