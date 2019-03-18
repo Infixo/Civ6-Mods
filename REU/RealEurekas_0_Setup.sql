ac048401d250087324146727f70881bf98e1e915
Infixo <swidwins@gmail.com>
2019-03-18 16:29:15 +0100

    REU: updated instructions

--- REU/RealEurekas_0_Setup.sql
+++ REU/RealEurekas_0_Setup.sql
@@ -13,10 +13,12 @@
 
 
 -- OPTION A - for SP & MP
--- This is an option for MultiPlayer games. Put any number you want instead of 987654321 and make sure that other players also use the same number.
+-- Use this option when you want to randomize boosts once and play entire game with this set.
+-- Enable it and put any number instead of 987654321. As long as this number stays the same, the mod will generate the same set of boosts.
+-- Different number will give a different set. Please use numbers that have 6 to 9 digits.
+-- This is an option for MultiPlayer games. Select a number and make sure that other players also use the same number.
 -- That way all players will have the same boosts and they will stay the same through the entire game.
 -- This option is also for SinglePlayer if you want to play entire game with the same set of boosts.
--- As long as the number is the same, the mod will select the same boosts. Different number will give a different set.
 
 --INSERT INTO GlobalParameters (Name, Value) VALUES ('REU_RANDOM_SEED', '987654321');
 
@@ -24,7 +26,8 @@
 
 -- OPTION B - for SP
 -- This option rolls randomly new set of boosts each time the game is started or re-loaded.
--- It is basically as the mod has been working so far. 
+-- It is basically the same as the mod has been working so far.
+-- If you don't mind that boosts change very often then use it.
 -- Please do NOT change anything in the statement (except comment ofc).
 
 --INSERT INTO GlobalParameters (Name, Value) VALUES ('REU_RANDOM_SEED', STRFTIME("%d%H%M%S"));
@@ -33,8 +36,8 @@
 
 -- OPTION C - for SP
 -- This option rolls randomly new set of boosts but it will change every day.
--- As long as you start a new game or reload a game within the same day - the boosts will be the same.
--- This option is for people you don't like when boosts change every time the game is reloaded, but would like some variety. New day - new boosts :)
+-- If you start a new game or reload a game within the same day - the boosts will be the same.
+-- This option is for people who don't like when boosts change every time the game is reloaded, but would like some variety. New day - new boosts :)
 -- Please do NOT change anything in the statement (except comment ofc).
 
 INSERT INTO GlobalParameters (Name, Value) VALUES ('REU_RANDOM_SEED', STRFTIME("%Y%m%d"));
