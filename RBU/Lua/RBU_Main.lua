237bb2bed79d79f6474b53324a9f84075a99d2b5
Infixo <swidwins@gmail.com>
2019-04-06 17:56:33 +0200

    RBU: added power plant upgrades

--- RBU/Lua/RBU_Main.lua
+++ RBU/Lua/RBU_Main.lua
@@ -3,16 +3,84 @@ print("Loading RBU_Main.lua from Real Building Upgrades version "..GlobalParamet
 -- Real Building Upgrades
 -- Author: Infixo
 -- 2018-03-30: Created
+-- 2019-04-06: Conversion projects for Power Plants
 --------------------------------------------------------------
 
+
+-- helper
+function GetTableIndex(sTable:string, sType:string)
+	return GameInfo[sTable][sType] and GameInfo[sTable][sType].Index or -1;
+end
+
+
 -- remove PALACE_UPGRADE if exists
+
+local BUILDING_PALACE_UPGRADE:number = GetTableIndex("Buildings", "BUILDING_PALACE_UPGRADE");
+
 function OnCityConquered(capturerID:number,  ownerID:number, cityID:number, cityX:number, cityY:number) -- cityID is already owned by capturerID
-	--print("OnCityConquered",capturerID,ownerID,cityID,cityX,cityY);
+	--print("FUN OnCityConquered",capturerID,ownerID,cityID,cityX,cityY);
 	local pCity:table = Cities.GetCityInPlot(cityX, cityY);
 	if not pCity then print("ERROR: OnCityConquered City not found", cityX, cityY); return; end
-	pCity:GetBuildings():RemoveBuilding(GameInfo.Buildings.BUILDING_PALACE_UPGRADE.Index);
+	pCity:GetBuildings():RemoveBuilding(BUILDING_PALACE_UPGRADE);
+	pCity:GetBuildQueue():RemoveBuilding(BUILDING_PALACE_UPGRADE);
 	--print("City has PU", pCity:GetBuildings():HasBuilding(GameInfo.Buildings.BUILDING_PALACE_UPGRADE.Index));
 end
-GameEvents.CityConquered.Add(OnCityConquered);
+Events.CityConquered.Add(OnCityConquered);
+
+
+-- 2019-04-06: Conversion projects for Power Plants
+-- CityProjectCompleted = { "player", "iCityID", "Project", "Building", "iX", "iY", "bCanceled" },
+
+-- projects
+local PROJECT_CONVERT_REACTOR_TO_COAL:number          = GetTableIndex("Projects", "PROJECT_CONVERT_REACTOR_TO_COAL");
+local PROJECT_CONVERT_REACTOR_TO_OIL:number           = GetTableIndex("Projects", "PROJECT_CONVERT_REACTOR_TO_OIL");
+local PROJECT_CONVERT_REACTOR_TO_URANIUM:number       = GetTableIndex("Projects", "PROJECT_CONVERT_REACTOR_TO_URANIUM");
+--local PROJECT_DECOMMISSION_COAL_POWER_PLANT:number    = GetTableIndex("Projects", "PROJECT_DECOMMISSION_COAL_POWER_PLANT");
+--local PROJECT_DECOMMISSION_OIL_POWER_PLANT:number     = GetTableIndex("Projects", "PROJECT_DECOMMISSION_OIL_POWER_PLANT");
+--local PROJECT_DECOMMISSION_NUCLEAR_POWER_PLANT:number = GetTableIndex("Projects", "PROJECT_DECOMMISSION_NUCLEAR_POWER_PLANT");
+
+-- power plants
+local BUILDING_COAL_POWER_PLANT_UPGRADE:number        = GetTableIndex("Buildings", "BUILDING_COAL_POWER_PLANT_UPGRADE");
+local BUILDING_FOSSIL_FUEL_POWER_PLANT_UPGRADE:number = GetTableIndex("Buildings", "BUILDING_FOSSIL_FUEL_POWER_PLANT_UPGRADE");
+local BUILDING_POWER_PLANT_UPGRADE:number             = GetTableIndex("Buildings", "BUILDING_POWER_PLANT_UPGRADE");
+
+function OnCityProjectCompleted(playerID:number, cityID:number, projectID:number)
+	print("FUN OnCityProjectCompleted", playerID, cityID, projectID);
+	print("... power plants upgrades BEFORE",
+		pCityBuildings:HasBuilding(BUILDING_COAL_POWER_PLANT_UPGRADE),
+		pCityBuildings:HasBuilding(BUILDING_FOSSIL_FUEL_POWER_PLANT_UPGRADE),
+		pCityBuildings:HasBuilding(BUILDING_POWER_PLANT_UPGRADE));
+	local pCity:table = Players[playerID]:GetCities():FindID(cityID);
+	if pCity == nil then print("ERROR: OnCityProjectCompleted city not found", playerID, cityID); return; end
+	local pCityBuildings:table = pCity:GetBuildings();
+	-- what to do
+	if projectID == PROJECT_CONVERT_REACTOR_TO_COAL then
+		if pCityBuildings:HasBuilding(BUILDING_FOSSIL_FUEL_POWER_PLANT_UPGRADE) or pCityBuildings:HasBuilding(BUILDING_POWER_PLANT_UPGRADE) then
+			pCity:GetBuildQueue():CreateBuilding(BUILDING_COAL_POWER_PLANT_UPGRADE);
+			pCityBuildings:RemoveBuilding(BUILDING_FOSSIL_FUEL_POWER_PLANT_UPGRADE);
+			pCityBuildings:RemoveBuilding(BUILDING_POWER_PLANT_UPGRADE);
+		end
+	elseif projectID == PROJECT_CONVERT_REACTOR_TO_OIL then
+		if pCityBuildings:HasBuilding(BUILDING_COAL_POWER_PLANT_UPGRADE) or pCityBuildings:HasBuilding(BUILDING_POWER_PLANT_UPGRADE) then
+			pCityBuildings:RemoveBuilding(BUILDING_COAL_POWER_PLANT_UPGRADE);
+			pCity:GetBuildQueue():CreateBuilding(BUILDING_FOSSIL_FUEL_POWER_PLANT_UPGRADE);
+			pCityBuildings:RemoveBuilding(BUILDING_POWER_PLANT_UPGRADE);
+		end
+	elseif projectID == PROJECT_CONVERT_REACTOR_TO_URANIUM then
+		if pCityBuildings:HasBuilding(BUILDING_COAL_POWER_PLANT_UPGRADE) or pCityBuildings:HasBuilding(BUILDING_FOSSIL_FUEL_POWER_PLANT_UPGRADE) then
+			pCityBuildings:RemoveBuilding(BUILDING_COAL_POWER_PLANT_UPGRADE);
+			pCityBuildings:RemoveBuilding(BUILDING_FOSSIL_FUEL_POWER_PLANT_UPGRADE);
+			pCity:GetBuildQueue():CreateBuilding(BUILDING_POWER_PLANT_UPGRADE);
+		end
+	--elseif projectID == PROJECT_DECOMMISSION_COAL_POWER_PLANT then
+	--elseif projectID == PROJECT_DECOMMISSION_OIL_POWER_PLANT then
+	--elseif projectID == PROJECT_DECOMMISSION_NUCLEAR_POWER_PLANT then
+	end
+	print("... power plants upgrades AFTER",
+		pCityBuildings:HasBuilding(BUILDING_COAL_POWER_PLANT_UPGRADE),
+		pCityBuildings:HasBuilding(BUILDING_FOSSIL_FUEL_POWER_PLANT_UPGRADE),
+		pCityBuildings:HasBuilding(BUILDING_POWER_PLANT_UPGRADE));
+end
+Events.CityProjectCompleted.Add(OnCityProjectCompleted);
 
 print("OK loaded RBU_Main.lua from Real Building Upgrades");
--- RBU/XP2/RBU_XP2.sql
+++ RBU/XP2/RBU_XP2.sql
@@ -9,9 +9,6 @@
 -- 2019-02-18 Changes to upgrades of Vanilla and R&F buildings
 --------------------------------------------------------------
 
--- 2019-02-18 Remove Power Plant Upgrade (temporarily, will be restored as an upgrade to the Nuclear Power Plant)
---DELETE FROM Buildings WHERE BuildingType = 'BUILDING_POWER_PLANT_UPGRADE';
-
 INSERT INTO Buildings_XP2 (BuildingType, RequiredPower, EntertainmentBonusWithPower) VALUES
 ('BUILDING_FACTORY_UPGRADE',             1, 0),
 ('BUILDING_ELECTRONICS_FACTORY_UPGRADE', 1, 0),
@@ -42,13 +39,6 @@ UPDATE Buildings SET OuterDefenseHitPoints =  50 WHERE BuildingType = 'BUILDING_
 UPDATE Buildings SET OuterDefenseHitPoints = 100 WHERE BuildingType = 'BUILDING_TSIKHE_UPGRADE';
 
 
---------------------------------------------------------------
--- 2019-04-06 Industrial Zone
--- Can’t give more power from resources because they consume dynamically. Focus on other effects.
---------------------------------------------------------------
-
--- Nuclear Power Plant needs a new name
-UPDATE Buildings SET Cost = 450, Name = 'LOC_BUILDING_NUCLEAR_POWER_PLANT_UPGRADE_NAME', Description = 'LOC_BUILDING_NUCLEAR_POWER_PLANT_UPGRADE_DESCRIPTION' WHERE BuildingType = 'BUILDING_POWER_PLANT_UPGRADE';
 -- Rearrange a bit so the techs will not get overcrowded
 UPDATE Buildings SET PrereqTech = 'TECH_REPLACEABLE_PARTS'   WHERE BuildingType = 'BUILDING_FACTORY_UPGRADE';
 UPDATE Buildings SET PrereqTech = 'TECH_NUCLEAR_FUSION'      WHERE BuildingType = 'BUILDING_POWER_PLANT_UPGRADE';
@@ -59,28 +49,179 @@ UPDATE Buildings SET PrereqCivic = 'CIVIC_MASS_MEDIA'       WHERE BuildingType =
 UPDATE Buildings SET PrereqCivic = 'CIVIC_MASS_MEDIA'       WHERE BuildingType = 'BUILDING_AQUARIUM_UPGRADE';
 UPDATE Buildings SET PrereqCivic = 'CIVIC_ENVIRONMENTALISM' WHERE BuildingType = 'BUILDING_AQUATICS_CENTER_UPGRADE';
 
-/*
-Coal 1 prod, 1 GEP, 4 power, rr6
-1 prod rr9 no, if RR possible :)
-2 prod from coal mines Or/and +1 coal generated
-Or, if possible, 1 prod from each tile with a RailRoad !
-Oil 2 peod, 1 GEP, 4 power, rr6
-1/prod rr9
-1 prod per district
-3 prod from oil wells, +1 oil accumulated
-Nuclear 4 prod, 3 science, 1 GEP, 16 power, rr6
-2 prod, 1 sci rr9
-2 prod per district
-4 prod from uranium mines, +1 uranium accumulated
-*/
+
+--------------------------------------------------------------
+-- 2019-04-06 Industrial Zone - power plants
+-- Can’t give more power from resources because they consume dynamically. Focus on other effects.
+--------------------------------------------------------------
+
+-- Nuclear Power Plant needs a new name
+UPDATE Buildings SET Cost = 450, Name = 'LOC_BUILDING_NUCLEAR_POWER_PLANT_UPGRADE_NAME', Description = 'LOC_BUILDING_NUCLEAR_POWER_PLANT_UPGRADE_DESCRIPTION' WHERE BuildingType = 'BUILDING_POWER_PLANT_UPGRADE';
+
+-- I can't do +1 from a resource, etc. because there is only EFFECT_ADJUST_CITY_EXTRA_ACCUMULATION_SPECIFIC_RESOURCE available
+-- thus I can only grant extra accumulation on a CITY level for city-level owners, like Buildings or Districts
+-- a) there is no EFFECT_ADJUST_EXTRA_ACCUMULATION_IMPROVEMENT nor EFFECT_ADJUST_EXTRA_ACCUMULATION_RESOURCE
+-- b) there is no requirement to check if a city has a specific plot or improvement
+-- new modifier type to adjust a specific strategic resource accumulation for a single city
+--INSERT INTO Types (Type, Kind)  -- hash value generated automatically
+--VALUES ('MODIFIER_SINGLE_CITY_ADJUST_EXTRA_ACCUMULATION_SPECIFIC_RESOURCE', 'KIND_MODIFIER');
+--INSERT INTO DynamicModifiers (ModifierType, CollectionType, EffectType)
+--VALUES ('MODIFIER_SINGLE_CITY_ADJUST_EXTRA_ACCUMULATION_SPECIFIC_RESOURCE', 'COLLECTION_OWNER', 'EFFECT_ADJUST_CITY_EXTRA_ACCUMULATION_SPECIFIC_RESOURCE');
+
+INSERT INTO MutuallyExclusiveBuildings (Building, MutuallyExclusiveBuilding) VALUES
+('BUILDING_COAL_POWER_PLANT_UPGRADE', 'BUILDING_FOSSIL_FUEL_POWER_PLANT'),
+('BUILDING_COAL_POWER_PLANT_UPGRADE', 'BUILDING_FOSSIL_FUEL_POWER_PLANT_UPGRADE'),
+('BUILDING_COAL_POWER_PLANT_UPGRADE', 'BUILDING_POWER_PLANT'),
+('BUILDING_COAL_POWER_PLANT_UPGRADE', 'BUILDING_POWER_PLANT_UPGRADE'),
+('BUILDING_FOSSIL_FUEL_POWER_PLANT_UPGRADE', 'BUILDING_COAL_POWER_PLANT'),
+('BUILDING_FOSSIL_FUEL_POWER_PLANT_UPGRADE', 'BUILDING_COAL_POWER_PLANT_UPGRADE'),
+('BUILDING_FOSSIL_FUEL_POWER_PLANT_UPGRADE', 'BUILDING_POWER_PLANT'),
+('BUILDING_FOSSIL_FUEL_POWER_PLANT_UPGRADE', 'BUILDING_POWER_PLANT_UPGRADE'),
+('BUILDING_POWER_PLANT_UPGRADE', 'BUILDING_FOSSIL_FUEL_POWER_PLANT'),
+('BUILDING_POWER_PLANT_UPGRADE', 'BUILDING_FOSSIL_FUEL_POWER_PLANT_UPGRADE'),
+('BUILDING_POWER_PLANT_UPGRADE', 'BUILDING_COAL_POWER_PLANT'),
+('BUILDING_POWER_PLANT_UPGRADE', 'BUILDING_COAL_POWER_PLANT_UPGRADE');
+
+--------------------------------------------------------------
+-- Projects to decomission power plants
+
+INSERT INTO Project_BuildingCosts (ProjectType, ConsumedBuildingType) VALUES
+('PROJECT_DECOMMISSION_COAL_POWER_PLANT',    'BUILDING_COAL_POWER_PLANT_UPGRADE'),
+('PROJECT_DECOMMISSION_OIL_POWER_PLANT',     'BUILDING_FOSSIL_FUEL_POWER_PLANT_UPGRADE'),
+('PROJECT_DECOMMISSION_NUCLEAR_POWER_PLANT', 'BUILDING_POWER_PLANT_UPGRADE');
+
+
+--------------------------------------------------------------
+-- COAL_POWER_PLANT, base: 1 prod, 1 GEP, 4 power, rr6
+
+-- RR=9
+UPDATE Buildings SET RegionalRange = 9
+WHERE BuildingType = 'BUILDING_COAL_POWER_PLANT_UPGRADE';
+
+-- +1 production
+INSERT INTO Building_YieldChanges (BuildingType, YieldType, YieldChange) VALUES
+('BUILDING_COAL_POWER_PLANT_UPGRADE', 'YIELD_PRODUCTION', 1);
+
+-- +1 Production for each specialty district constructed
+INSERT INTO BuildingModifiers (BuildingType, ModifierId) VALUES
+('BUILDING_COAL_POWER_PLANT_UPGRADE', 'POWER_PLANT_UPGRADE_1_PRODUCTION_PER_DISTRICT');
+
+INSERT INTO Modifiers (ModifierId, ModifierType, RunOnce, Permanent, OwnerRequirementSetId, SubjectRequirementSetId) VALUES
+('POWER_PLANT_UPGRADE_1_PRODUCTION_PER_DISTRICT', 'MODIFIER_SINGLE_CITY_ADJUST_CITY_YIELD_PER_DISTRICT', 0, 0, NULL, NULL);
+
+INSERT INTO ModifierArguments (ModifierId, Name, Value) VALUES
+('POWER_PLANT_UPGRADE_1_PRODUCTION_PER_DISTRICT', 'YieldType', 'YIELD_PRODUCTION'),
+('POWER_PLANT_UPGRADE_1_PRODUCTION_PER_DISTRICT', 'Amount',    '1');
+
+-- Coal Mines: +3 Production
+INSERT INTO BuildingModifiers (BuildingType, ModifierId) VALUES
+('BUILDING_COAL_POWER_PLANT_UPGRADE', 'COAL_POWER_PLANT_UPGRADE_ADD_COAL_MINE_PRODUCTION');
+
+INSERT INTO Modifiers (ModifierId, ModifierType, RunOnce, Permanent, OwnerRequirementSetId, SubjectRequirementSetId) VALUES
+('COAL_POWER_PLANT_UPGRADE_ADD_COAL_MINE_PRODUCTION', 'MODIFIER_CITY_PLOT_YIELDS_ADJUST_PLOT_YIELD', 0, 0, NULL, 'PLOT_HAS_COAL_MINE_REQUIREMENTS');
+
+INSERT INTO ModifierArguments (ModifierId, Name, Value) VALUES
+('COAL_POWER_PLANT_UPGRADE_ADD_COAL_MINE_PRODUCTION', 'YieldType', 'YIELD_PRODUCTION'),
+('COAL_POWER_PLANT_UPGRADE_ADD_COAL_MINE_PRODUCTION', 'Amount',    '3');
+
+INSERT INTO RequirementSets (RequirementSetId, RequirementSetType) VALUES
+('PLOT_HAS_COAL_MINE_REQUIREMENTS', 'REQUIREMENTSET_TEST_ALL');
+
+INSERT INTO RequirementSetRequirements (RequirementSetId, RequirementId) VALUES
+('PLOT_HAS_COAL_MINE_REQUIREMENTS', 'REQUIRES_COAL_IN_PLOT'),
+('PLOT_HAS_COAL_MINE_REQUIREMENTS', 'REQUIRES_PLOT_HAS_MINE'); -- exists
+
+INSERT INTO Requirements (RequirementId, RequirementType) VALUES
+('REQUIRES_COAL_IN_PLOT', 'REQUIREMENT_PLOT_RESOURCE_TYPE_MATCHES');
+
+INSERT INTO RequirementArguments (RequirementId, Name, Value) VALUES
+('REQUIRES_COAL_IN_PLOT', 'ResourceType', 'RESOURCE_COAL');
+
+
+--------------------------------------------------------------
+-- FOSSIL_FUEL_POWER_PLANT, base: 2 prod, 1 GEP, 4 power, rr6
+
+-- RR=9
+UPDATE Buildings SET RegionalRange = 9
+WHERE BuildingType = 'BUILDING_FOSSIL_FUEL_POWER_PLANT_UPGRADE';
+
+-- +1 Production
+INSERT INTO Building_YieldChanges (BuildingType, YieldType, YieldChange) VALUES
+('BUILDING_FOSSIL_FUEL_POWER_PLANT_UPGRADE', 'YIELD_PRODUCTION', 1);
+
+-- +1 Production for each specialty district constructed
+INSERT INTO BuildingModifiers (BuildingType, ModifierId) VALUES
+('BUILDING_FOSSIL_FUEL_POWER_PLANT_UPGRADE', 'POWER_PLANT_UPGRADE_1_PRODUCTION_PER_DISTRICT'); -- defined for a Coal Power Plant Upgrade
+
+-- +3 Production from Oil Wells
+INSERT INTO BuildingModifiers (BuildingType, ModifierId) VALUES
+('BUILDING_FOSSIL_FUEL_POWER_PLANT_UPGRADE', 'POWER_PLANT_UPGRADE_ADD_OILWELL_PRODUCTION'); -- defined for the vanilla Power Plant, re-used here
+
+
+--------------------------------------------------------------
+-- NUCLEAR_POWER_PLANT, base: Nuclear 4 prod, 3 science, 1 GEP, 16 power, rr6
+
+-- RR=9 - already defined
+-- +2 Production - already defined
+
+-- +2 Production, +1 Science
+INSERT INTO Building_YieldChanges (BuildingType, YieldType, YieldChange) VALUES
+('BUILDING_POWER_PLANT_UPGRADE', 'YIELD_SCIENCE', 1);
+
+-- +2 Production for each specialty district constructed - already defined
+
+-- +3 prod from Oil Well - remove, as it is now for Oil Power Plant Upgrade
+DELETE FROM BuildingModifiers WHERE BuildingType = 'BUILDING_POWER_PLANT_UPGRADE' AND ModifierId = 'POWER_PLANT_UPGRADE_ADD_OILWELL_PRODUCTION';
+
+-- +4 Production, +2 Science from Uranium Mines
+INSERT INTO BuildingModifiers (BuildingType, ModifierId) VALUES
+('BUILDING_POWER_PLANT_UPGRADE', 'POWER_PLANT_UPGRADE_ADD_URANIUM_MINE_PRODUCTION'),
+('BUILDING_POWER_PLANT_UPGRADE', 'POWER_PLANT_UPGRADE_ADD_URANIUM_MINE_SCIENCE');
+
+INSERT INTO Modifiers (ModifierId, ModifierType, RunOnce, Permanent, OwnerRequirementSetId, SubjectRequirementSetId) VALUES
+('POWER_PLANT_UPGRADE_ADD_URANIUM_MINE_PRODUCTION', 'MODIFIER_CITY_PLOT_YIELDS_ADJUST_PLOT_YIELD', 0, 0, NULL, 'PLOT_HAS_URANIUM_MINE_REQUIREMENTS'),
+('POWER_PLANT_UPGRADE_ADD_URANIUM_MINE_SCIENCE',    'MODIFIER_CITY_PLOT_YIELDS_ADJUST_PLOT_YIELD', 0, 0, NULL, 'PLOT_HAS_URANIUM_MINE_REQUIREMENTS');
+
+INSERT INTO ModifierArguments (ModifierId, Name, Value) VALUES
+('POWER_PLANT_UPGRADE_ADD_URANIUM_MINE_PRODUCTION', 'YieldType', 'YIELD_PRODUCTION'),
+('POWER_PLANT_UPGRADE_ADD_URANIUM_MINE_PRODUCTION', 'Amount',    '4'),
+('POWER_PLANT_UPGRADE_ADD_URANIUM_MINE_SCIENCE', 'YieldType', 'YIELD_SCIENCE'),
+('POWER_PLANT_UPGRADE_ADD_URANIUM_MINE_SCIENCE', 'Amount',    '2');
+
+INSERT INTO RequirementSets (RequirementSetId, RequirementSetType) VALUES
+('PLOT_HAS_URANIUM_MINE_REQUIREMENTS', 'REQUIREMENTSET_TEST_ALL');
+
+INSERT INTO RequirementSetRequirements (RequirementSetId, RequirementId) VALUES
+('PLOT_HAS_URANIUM_MINE_REQUIREMENTS', 'REQUIRES_URANIUM_IN_PLOT'), -- defined for BUILDING_RESEARCH_LAB_UPGRADE
+('PLOT_HAS_URANIUM_MINE_REQUIREMENTS', 'REQUIRES_PLOT_HAS_MINE'); -- exists
+
 
 
 --------------------------------------------------------------
 -- 2019-04-06 Dam
 --------------------------------------------------------------
 
-/*
-Hydro 6 power
-+2 power
-Production to all river tiles? Check RND.
-*/
+-- +2 power
+-- Merchant's promo does not apply to the upgrade (because there is no new source in the city)
+INSERT INTO BuildingModifiers (BuildingType, ModifierId) VALUES
+('BUILDING_HYDROELECTRIC_DAM_UPGRADE', 'HYDROELECTRIC_DAM_UPGRADE_FREE_POWER');
+
+-- Important: MODIFIER_CITY_PLOT_YIELDS_ADJUST_PLOT_YIELD - tiles with Districts DON'T get any bonus (e.g. City Center nor IZ where Dam was built);	
+INSERT INTO Modifiers (ModifierId, ModifierType, RunOnce, Permanent, OwnerRequirementSetId, SubjectRequirementSetId) VALUES
+('HYDROELECTRIC_DAM_UPGRADE_FREE_POWER', 'MODIFIER_SINGLE_CITY_ADJUST_FREE_POWER', 0, 0, NULL, NULL);
+
+INSERT INTO ModifierArguments (ModifierId, Name, Value) VALUES
+('HYDROELECTRIC_DAM_UPGRADE_FREE_POWER', 'SourceType', 'FREE_POWER_SOURCE_WATER'),
+('HYDROELECTRIC_DAM_UPGRADE_FREE_POWER', 'Amount',     '2');
+
+-- +1 Production from tiles adjacent to the river
+INSERT INTO BuildingModifiers (BuildingType, ModifierId) VALUES
+('BUILDING_HYDROELECTRIC_DAM_UPGRADE', 'HYDROELECTRIC_DAM_UPGRADE_ADD_PRODUCTION');
+
+-- Important: MODIFIER_CITY_PLOT_YIELDS_ADJUST_PLOT_YIELD - tiles with Districts DON'T get any bonus (e.g. City Center nor IZ where Dam was built);	
+INSERT INTO Modifiers (ModifierId, ModifierType, RunOnce, Permanent, OwnerRequirementSetId, SubjectRequirementSetId) VALUES
+('HYDROELECTRIC_DAM_UPGRADE_ADD_PRODUCTION', 'MODIFIER_CITY_PLOT_YIELDS_ADJUST_PLOT_YIELD', 0, 0, NULL, 'PLOT_ADJACENT_TO_RIVER_REQUIREMENTS'); -- can't set permanent because can be pillaged
+
+INSERT INTO ModifierArguments (ModifierId, Name, Value) VALUES
+('HYDROELECTRIC_DAM_UPGRADE_ADD_PRODUCTION', 'YieldType', 'YIELD_PRODUCTION'),
+('HYDROELECTRIC_DAM_UPGRADE_ADD_PRODUCTION', 'Amount',    '1');
