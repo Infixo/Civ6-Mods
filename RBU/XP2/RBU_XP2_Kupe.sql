f9f0fb9e2338c509d80f9b649b2137073ecc9dbb
Infixo <swidwins@gmail.com>
2019-04-06 22:37:42 +0200

    RBU: marae upgrade

--- RBU/XP2/RBU_XP2_Kupe.sql
+++ RBU/XP2/RBU_XP2_Kupe.sql
@@ -7,26 +7,57 @@
 
 --------------------------------------------------------------
 -- MARAE
-/*
-
-1 amenity like Amphi
-+1 production for each feature that could be removed (woods, jungle, marshes, etc.), tourism and gold from non-removables
-+1 tourism and +2 gold for each feature on tiles with high appeal and Reefs
-Central to identity - +2 loyalty in the city
-
 
 INSERT INTO BuildingReplaces (CivUniqueBuildingType, ReplacesBuildingType) VALUES
-('BUILDING_TSIKHE_UPGRADE', 'BUILDING_STAR_FORT_UPGRADE');
+('BUILDING_MARAE_UPGRADE', 'BUILDING_AMPHITHEATER_UPGRADE');
 
-UPDATE Buildings SET TraitType = (SELECT TraitType FROM Buildings WHERE BuildingType = 'BUILDING_TSIKHE') -- TRAIT_CIVILIZATION_BUILDING_TSIKHE
-WHERE BuildingType = 'BUILDING_TSIKHE_UPGRADE';
+UPDATE Buildings SET TraitType = 'TRAIT_CIVILIZATION_BUILDING_MARAE'
+WHERE BuildingType = 'BUILDING_MARAE_UPGRADE';
 
--- +1 Amenity & Housing, +25 Defense
-UPDATE Buildings
-SET PurchaseYield = NULL, OuterDefenseHitPoints = 25, OuterDefenseStrength = 1, Entertainment = 1, Housing = 1
-WHERE BuildingType = 'BUILDING_TSIKHE_UPGRADE';
+-- +1 Amenity
+UPDATE Buildings SET Entertainment = 1
+WHERE BuildingType = 'BUILDING_MARAE_UPGRADE';
 
--- +1 Faith
-INSERT INTO Building_YieldChanges (BuildingType, YieldType, YieldChange) VALUES -- generated from Excel
-('BUILDING_TSIKHE_UPGRADE', 'YIELD_FAITH', 1);
-*/
+-- +0.2 culture per pop as Amphitheater
+INSERT INTO BuildingModifiers (BuildingType, ModifierId) VALUES
+('BUILDING_MARAE_UPGRADE', 'AMPHITHEATER_UPGRADE_ADJUST_CULTURE_PER_POP');
+
+-- +1 Loyalty, central to identity
+INSERT INTO BuildingModifiers (BuildingType, ModifierId) VALUES
+('BUILDING_MARAE_UPGRADE', 'MARAE_UPGRADE_LOYALTY');
+
+INSERT INTO Modifiers (ModifierId, ModifierType, RunOnce, Permanent, OwnerRequirementSetId, SubjectRequirementSetId) VALUES
+('MARAE_UPGRADE_LOYALTY', 'MODIFIER_SINGLE_CITY_ADJUST_IDENTITY_PER_TURN', 0, 0, NULL, NULL);
+
+INSERT INTO ModifierArguments (ModifierId, Name, Value) VALUES
+('MARAE_UPGRADE_LOYALTY', 'Amount', '1');
+
+-- +1 Production from Marsh and unimproved Woods and Rainforest
+INSERT INTO BuildingModifiers (BuildingType, ModifierId) VALUES
+('BUILDING_MARAE_UPGRADE', 'MARAE_UPGRADE_ADD_FOREST_PRODUCTION'),
+('BUILDING_MARAE_UPGRADE', 'MARAE_UPGRADE_ADD_JUNGLE_PRODUCTION'),
+('BUILDING_MARAE_UPGRADE', 'MARAE_UPGRADE_ADD_MARSH_PRODUCTION');
+
+INSERT INTO Modifiers (ModifierId, ModifierType, RunOnce, Permanent, OwnerRequirementSetId, SubjectRequirementSetId) VALUES
+('MARAE_UPGRADE_ADD_FOREST_PRODUCTION', 'MODIFIER_CITY_PLOT_YIELDS_ADJUST_PLOT_YIELD', 0, 0, NULL, 'PLOT_HAS_FOREST_NO_IMPROVEMENT_REQUIREMENTS'),
+('MARAE_UPGRADE_ADD_JUNGLE_PRODUCTION', 'MODIFIER_CITY_PLOT_YIELDS_ADJUST_PLOT_YIELD', 0, 0, NULL, 'PLOT_HAS_JUNGLE_NO_IMPROVEMENT_REQUIREMENTS'),
+('MARAE_UPGRADE_ADD_MARSH_PRODUCTION',  'MODIFIER_CITY_PLOT_YIELDS_ADJUST_PLOT_YIELD', 0, 0, NULL, 'ZOO_MARSH_REQUIREMENTS'); -- !!!ZOO!!!
+
+INSERT INTO ModifierArguments (ModifierId, Name, Value) VALUES
+('MARAE_UPGRADE_ADD_FOREST_PRODUCTION', 'YieldType', 'YIELD_PRODUCTION'),
+('MARAE_UPGRADE_ADD_FOREST_PRODUCTION', 'Amount',    '1'),
+('MARAE_UPGRADE_ADD_JUNGLE_PRODUCTION', 'YieldType', 'YIELD_PRODUCTION'),
+('MARAE_UPGRADE_ADD_JUNGLE_PRODUCTION', 'Amount',    '1'),
+('MARAE_UPGRADE_ADD_MARSH_PRODUCTION',  'YieldType', 'YIELD_PRODUCTION'),
+('MARAE_UPGRADE_ADD_MARSH_PRODUCTION',  'Amount',    '1');
+
+-- +2 Gold from FEATURE_REEF
+INSERT INTO BuildingModifiers (BuildingType, ModifierId) VALUES
+('BUILDING_MARAE_UPGRADE', 'MARAE_UPGRADE_ADD_REEF_GOLD');
+
+INSERT INTO Modifiers (ModifierId, ModifierType, RunOnce, Permanent, OwnerRequirementSetId, SubjectRequirementSetId) VALUES
+('MARAE_UPGRADE_ADD_REEF_GOLD', 'MODIFIER_CITY_PLOT_YIELDS_ADJUST_PLOT_YIELD', 0, 0, NULL, 'AQUARIUM_REEF_REQUIREMENTS'); -- !!!AQUARIUM!!!
+
+INSERT INTO ModifierArguments (ModifierId, Name, Value) VALUES
+('MARAE_UPGRADE_ADD_REEF_GOLD', 'YieldType', 'YIELD_GOLD'),
+('MARAE_UPGRADE_ADD_REEF_GOLD', 'Amount',    '2');
