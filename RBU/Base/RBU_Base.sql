66711246dbbfd72521d44bd28c56274e84d60b5a
Infixo <swidwins@gmail.com>
2019-07-15 14:21:47 +0200

    RBU: Version 4.1.1
    Defense buildings now provide +1 strength (was +2)

--- RBU/Base/RBU_Base.sql
+++ RBU/Base/RBU_Base.sql
@@ -150,7 +150,7 @@ WHERE BuildingType = 'BUILDING_SEWER_UPGRADE';
 -- WALLS / CASTLE / STAR_FORT (exceptions!)
 
 -- +1 Housing, +50 HP
-UPDATE Buildings SET PurchaseYield = NULL, OuterDefenseHitPoints = 50, OuterDefenseStrength = 2, Housing = 1
+UPDATE Buildings SET PurchaseYield = NULL, OuterDefenseHitPoints = 50, OuterDefenseStrength = 1, Housing = 1
 WHERE BuildingType IN ('BUILDING_CASTLE_UPGRADE', 'BUILDING_STAR_FORT_UPGRADE', 'BUILDING_WALLS_UPGRADE');
 
 
