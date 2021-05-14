ab54bdf581efb909353ce00aaa5a7be48a8df7e2
Infixo <swidwins@gmail.com>
2021-05-15 01:34:24 +0200

    BST: Version 1.0

---
+++ BST/xp2/citystates_bst_xp2.lua
@@ -0,0 +1,6 @@
+-- ===========================================================================
+-- Better City States
+-- Author: Infixo
+-- This file is needed for compatibility with CQUI
+-- ===========================================================================
+include("CityStates_Expansion2");
